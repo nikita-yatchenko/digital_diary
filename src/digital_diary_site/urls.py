31959a5d78b240a7ec2cc9e6327320b356ba5ece
Ismail <113051912+kitsuyome@users.noreply.github.com>
2022-12-19 04:40:44 +0300

    Update urls.py

--- src/digital_diary_site/urls.py
+++ src/digital_diary_site/urls.py
@@ -21,3 +21,9 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('vis/', include('visualization.urls'))
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
